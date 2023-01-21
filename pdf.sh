d9822b5b00247adc4317d0b02bcfd252b864df56
trubiso <trubiso@users.noreply.github.com>
2023-01-21 11:02:03 +0100

    new pdf.sh to pull own dotfiles into df/

---
+++ pdf.sh
@@ -0,0 +1,12 @@
+# pulls dotfiles
+cp $HOME/.zshrc                         df/.zshrc
+
+cp $HOME/.config/monitors.sh            df/monitors.sh
+
+cp $HOME/.config/i3/config              df/i3
+ 
+cp $HOME/.config/i3status/config        df/i3status 
+
+cp $HOME/.config/nvim/init.vim          df/init.vim 
+
+cp $HOME/.config/nvim/coc-settings.json df/coc-settings.json 
