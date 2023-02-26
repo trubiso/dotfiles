05312d634a7ecc78fa6718af3b2aebc177fd289d
trubiso <trubiso@users.noreply.github.com>
2023-02-27 00:25:05 +0100

    polybar fix on ubuntu

--- df/polybar.sh
+++ df/polybar.sh
@@ -1,7 +1,7 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar left &
-polybar master &
-polybar right &
+polybar left --config=$HOME/.config/polybar/config.ini &
+polybar master --config=$HOME/.config/polybar/config.ini &
+polybar right --config=$HOME/.config/polybar/config.ini &
 
